599b579231809ed7e01b65849d093877ee167d86
Dan Bright <brahyt@gmail.com>
2025-09-09 13:37:50 -0700

    enums baby, loveem

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use chrono::{Duration, Local};
 use clap::Parser;
 use diesel::prelude::*;
+use diesel::pg::PgConnection;
 use dun::models::{NewTask, Task};
 use dun::schema::tasks;
 use dun::schema::tasks::created_at;
@@ -29,6 +30,13 @@ fn format_for_claude(tasks: &[String]) {
     child.wait().expect("Failed to wait for claude command");
 }
 
+#[derive(Debug)]
+enum QueryMode {
+    Add(String),
+    Today { use_claude: bool },
+    Yesterday { use_claude: bool },
+}
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 
@@ -46,54 +54,74 @@ struct Args {
     today: bool,
 }
 
+fn determine_query_mode(args: &Args) -> Option<QueryMode> {
+    if let Some(did) = &args.did {
+        Some(QueryMode::Add(did.clone()))
+    } else if args.today {
+        Some(QueryMode::Today { use_claude: args.claude })
+    } else if args.yesterday {
+        Some(QueryMode::Yesterday { use_claude: args.claude })
+    } else {
+        None
+    }
+}
+
+fn handle_query_mode(mode: QueryMode, db: &mut PgConnection) {
+    match mode {
+        QueryMode::Add(msg) => {
+            let new_task = NewTask { message: &msg };
+
+            diesel::insert_into(tasks::table)
+                .values(&new_task)
+                .returning(Task::as_returning())
+                .get_result::<Task>(db)
+                .expect("Error saving new post");
+
+            println!("You did: {}", msg);
+        }
+        QueryMode::Today { use_claude } => {
+            let today = Local::now().naive_local().date();
+            let today_start = today.and_hms_opt(0, 0, 0).unwrap();
+            let tomorrow_start = (today + Duration::days(1)).and_hms_opt(0, 0, 0).unwrap();
+
+            let task_messages = tasks::table
+                .select(message)
+                .filter(created_at.ge(today_start).and(created_at.lt(tomorrow_start)))
+                .load::<String>(db)
+                .expect("Error");
+
+            if use_claude {
+                format_for_claude(&task_messages);
+            } else {
+                println!("{:?}", task_messages);
+            }
+        }
+        QueryMode::Yesterday { use_claude } => {
+            let today = Local::now().naive_local().date();
+            let yesterday_start = (today - Duration::days(1)).and_hms_opt(0, 0, 0).unwrap();
+            let today_start = today.and_hms_opt(0, 0, 0).unwrap();
+
+            let task_messages = tasks::table
+                .select(message)
+                .filter(created_at.ge(yesterday_start).and(created_at.lt(today_start)))
+                .load::<String>(db)
+                .expect("Error");
+
+            if use_claude {
+                format_for_claude(&task_messages);
+            } else {
+                println!("{:?}", task_messages);
+            }
+        }
+    }
+}
+
 fn main() {
     let mut db = establish_connection();
     let args = Args::parse();
 
-    if let Some(did) = args.did {
-        let new_task = NewTask { message: &did };
-
-        diesel::insert_into(tasks::table)
-            .values(&new_task)
-            .returning(Task::as_returning())
-            .get_result::<Task>(&mut db)
-            .expect("Error saving new post");
-
-        println!("You did: {}", did);
-    } else if args.today {
-        let today = Local::now().naive_local().date();
-        let today_start = today.and_hms_opt(0, 0, 0).unwrap();
-        let tomorrow_start = (today + Duration::days(1)).and_hms_opt(0, 0, 0).unwrap();
-
-        let task_messages = tasks::table
-            .select(message)
-            .filter(created_at.ge(today_start).and(created_at.lt(tomorrow_start)))
-            .load::<String>(&mut db)
-            .expect("Error");
-
-        if args.claude {
-            format_for_claude(&task_messages);
-        } else {
-            println!("{:?}", task_messages);
-        }
-    } else if args.yesterday {
-        let today = Local::now().naive_local().date();
-        let yesterday_start = (today - Duration::days(1)).and_hms_opt(0, 0, 0).unwrap();
-        let today_start = today.and_hms_opt(0, 0, 0).unwrap();
-
-        let task_messages = tasks::table
-            .select(message)
-            .filter(created_at.ge(yesterday_start).and(created_at.lt(today_start)))
-            .load::<String>(&mut db)
-            .expect("Error");
-
-        if args.claude {
-            format_for_claude(&task_messages);
-        } else {
-            println!("{:?}", task_messages);
-        }
-        // Print out yesterdays events
-    } else {
-        println!("Gotta put something");
+    match determine_query_mode(&args) {
+        Some(mode) => handle_query_mode(mode, &mut db),
+        None => println!("Gotta put something"),
     }
 }
